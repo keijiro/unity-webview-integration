b6be39d0496342783f0b782ed182f7681eab9d88
Keijiro Takahashi <keijiro@gmail.com>
2011-12-28 17:15:19 +0900

    Update unity.js

--- unity.js
+++ unity.js
@@ -1,5 +1,6 @@
 function UnityWebMediator() {
     this.android = navigator.userAgent.match(/Android/);
+    this.ios = navigator.userAgent.match(/iOS/);
 
     this.callback = function(path, args) {
         var message = path;
@@ -14,6 +15,8 @@ function UnityWebMediator() {
         
         if (this.android) {
             UnityInterface.pushMessage(message);
+        } else if (this.android) {
+            window.location = "unity://callback" + message;
         } else {
             console.log(message);
         }
